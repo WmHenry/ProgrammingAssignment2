de0f47c1daf5929f5c65ec6a89c14ebb66597105
WmHenry <wmhstats@gmail.com>
2016-06-09 10:40:00 -0400

    Two functions that cache the inverse of a matrix
    Programming Assignment 2: Lexical Scoping.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+    # syntax orgenized to be visually clean. 
+    # two functions that cache for the inverse of a matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix())   {
+  cachedInverse <- NULL
+  setInv <- function(y)                       {
+    
+    # nested objects 'x', 'cashedInverse' in set environment.
+    
+    x <<- y
+    cachedInverse <<- NULL
+  }
+  
+  getnested <- function() x
+  setInverse<- function(inverse) cachedInverse <<- inverse
+  getInverse<- function() cachedInverse
+  
+  list(setInv = setInv, getnested = getnested,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+# function for the inverse of an cacheMatrix object
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)   {
+  
+  # matrix return that is the inverse of 'x'
+  
+  invFunc <- x$getInverse()
+  if(!is.null(invFunc))        {
+    message("getting cached data")
+    
+    # returning the 'invFunc' object
+    
+    return(invFunc)
+  }
+  data <- x$getnested()
+  invFunc <- solve(data, ...)
+  x$setInverse(invFunc)
+  invFunc
 }
